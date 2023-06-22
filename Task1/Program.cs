954d67dec6583a9d2e643fb2422bfaaf7c701372
Nikita <varashilov125@gmail.com>
2023-06-22 12:31:43 +0300

    Task1 цыкл возведения в степень

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B: ");
+int limit = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {A} возведённое в натуральную степень {GetSt(limit)}");
+
+
+int GetSt(int B)
+        {
+            int st = 1;
+            for(int i = 1; i<=B; i++){
+                st = st * A;
+            }
+            return st;
+        }
