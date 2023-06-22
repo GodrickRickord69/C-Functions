18fa9ce37c170b680aa4b8f90200dcdb7cec159f
Nikita <varashilov125@gmail.com>
2023-06-22 12:51:07 +0300

    Task2 сумма цифр числа

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Введите число: ");
+int limit = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр числа {limit} равна {GetSumNumb(limit)}");
+
+int GetSumNumb(int A){
+
+    int sn = 0;
+    while(A != 0){
+            sn += A % 10;
+            A /= 10;
+    }
+    return sn;
+};
+
